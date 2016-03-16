190cb99c40d7516f7312af446d3b12f4eea7dfe4
George F Rogers <grogers385@gmail.com>
2016-03-16 09:25:27 -0500

    update

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	on := flag.String("on", ":8080", "on")
 	flag.Parse()
+	http.Handle("/static/", http.FileServer(http.Dir("public")))
 	http.Handle("/", app.App)
 	log.Println("Listening on:", *on)
 	log.Fatal(http.ListenAndServe(*on, nil))
